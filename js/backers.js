226054a7ca774040c7ffb50756c5786b0516ce44
maheshsapre <mahesh.sapre@connovatech.com>
2014-07-11 17:50:25 +0530

    Added PFC data format export option to backers page

--- js/backers.js
+++ js/backers.js
@@ -55,6 +55,13 @@ function onSearchBacker() {
 	if ($.trim($("#searchNotCountry").val()).length > 0) url = url + "&q[addresses_country_not_cont]="+ encodeURIComponent($.trim($("#searchNotCountry").val()));
 	if ($.trim($("#searchPerkName").val()).length > 0) url = url + "&q[orders_perks_name_cont]="+ encodeURIComponent($.trim($("#searchPerkName").val()));
 	if ($.trim($("#searchOrderReference").val()).length > 0) url = url + "&q[orders_reference_no_cont]="+ encodeURIComponent($.trim($("#searchOrderReference").val()));    	
+
+	if ($.trim($("#shipping_date_gteq").val()).length > 0) url = url + "&q[shipping_date_gteq]="+ encodeURIComponent($.trim($("#shipping_date_gteq").val()));    	
+	if ($.trim($("#shipping_date_lteq").val()).length > 0) url = url + "&q[shipping_date_lteq]="+ encodeURIComponent($.trim($("#shipping_date_lteq").val()));    	
+
+
+	if ($.trim($("#data_format").val()).length > 0) url = url + "&data_format="+ encodeURIComponent($.trim($("#data_format").val()));    	
+	if ($.trim($("#report_to").val()).length > 0) url = url + "&report_to="+ encodeURIComponent($.trim($("#report_to").val()));    	
 	
 	window.location="backers.html?" + url + "&page=1&search=1&size=100000"  ;
 }
@@ -65,6 +72,12 @@ function searchBackerInfo() {
 	$("#searchNotCountry").val(decodeURIComponent(urlParameterValue('q[addresses_country_not_cont]')));
 	$("#searchOrderReference").val(decodeURIComponent(urlParameterValue('q[orders_reference_no_cont]')));
 	$("#searchPerkName").val(decodeURIComponent(urlParameterValue('q[orders_perks_name_cont]')))
+	$("#shipping_date_gteq").val(decodeURIComponent(urlParameterValue('q[shipping_date_gteq]')))
+	$("#shipping_date_lteq").val(decodeURIComponent(urlParameterValue('q[shipping_date_lteq]')))
+
+
+	$("#data_format").val(decodeURIComponent(urlParameterValue('data_format')))
+	$("#report_to").val(decodeURIComponent(urlParameterValue('report_to')))
 
 	switch (urlParameterValue('q[address_confirmed_eq]'))
 	{
@@ -94,7 +107,15 @@ $("#loading").hide();
 
 function onSuccessSearchGetBackerInfo(response) {  
 	$("#loading").hide();
-	drawDatatable(response.data);
+	if (response.data == "Email sent")
+	{
+			alert("Search result is sent to the specified email.");
+	}
+	else
+	{
+		drawDatatable(response.data);
+	}
+
 }
 
 
